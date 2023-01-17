abf3d1a9f1355de619c3bb46a770b4769766d143
lucascapocasa1 <86982967+lucascapocasa1@users.noreply.github.com>
2023-01-17 16:33:01 -0300

    Add files via upload

---
+++ comportamientos.js
@@ -0,0 +1,12 @@
+function redireccionarALinkedin(){
+    window.location.href = "https://www.linkedin.com/home"
+}
+
+function redireccionarAFarmacity(){
+    window.location.href = "https://www.farmacity.com/"
+
+}
+
+function redireccionarABancoSantander(){
+    window.location.href = "https://www.santander.com.ar/banco/online/personas"
+}
